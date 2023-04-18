9a36aa9287f7084d36e8586d2cf1a729af102caa
Alexander Olegård <aolegard@kth.se>
2023-04-18 08:47:03 +0200

    first presenter

---
+++ Presenter/frontPagePresenter.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default
+    function FrontPage(props) {
+
+    const [test, setTest] = React.useState(props.model.dataArray);
+    React.useEffect(wasCreatedACB, []);
+
+    function observerACB() {
+        setTest(props.model.dataArray)
+    }
+
+    function wasCreatedACB() {
+        props.model.addObserver(observerACB);
+        function isTakenDownACB() {
+            props.model.removeObserver(observerACB);
+        }
+        return isTakenDownACB;
+    }
+
+    function changeTest(number) {
+        props.model.addData(number);
+    }
+
+    return (
+            <FrontpageView
+            test={test}
+            onChangedTest={changeTest}
+            >
+            </FrontpageView>
+    )
+}
