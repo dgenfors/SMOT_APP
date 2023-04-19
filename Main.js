2ec78c7b5a7b6498b619ec9a559d4401a21b14ae
Alexander Olegård <aolegard@kth.se>
2023-04-19 16:22:10 +0200

    added actual signout to top button

--- Main.js
+++ Main.js
@@ -9,23 +9,32 @@ import { ModelContext } from "./ModelContext";
 export default function Main(props) {
   const Stack = props.stack;
 
-  function logOut(navigation){
+  function logOut(navigation) {
     popToTop();
   }
 
-  
+
   return (
     <NavigationContainer>
       <ModelContext.Provider value={props.model}>
         <View style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }}>
           <Stack.Navigator initialRouteName="Login">
-            <Stack.Screen name="Front" component={FrontPage} options={({navigation, route}) => ({
-            headerBackVisible: false,
-            headerRight: () => (<Button onPress={() => navigation.popToTop()} title="Logout"/>)})} />
+            <Stack.Screen name="Front" component={FrontPage} options={({ navigation, route }) => ({
+              headerBackVisible: false,
+              headerRight: () => (<Button onPress={() => {
+                navigation.popToTop()
+                try {
+                  signOut(auth)
+                  console.log("signed out");
+                } catch (error) {
+                  console.log(error.message)
+                }
+              }} title="Logout" />)
+            })} />
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="SignUp" component={SignUpPage} />
 
-            
+
 
           </Stack.Navigator>
         </View>
