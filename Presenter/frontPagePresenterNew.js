6f97e0976afd9f842147331fc4412600a7a530ed
AlexanderOlegard <zanru.wankelao@gmail.com>
2023-04-25 10:04:01 +0200

    made a new frontpage presenter

---
+++ Presenter/frontPagePresenterNew.js
@@ -0,0 +1,48 @@
+import React from "react";
+import FrontpageView from "../Views/frontpageView";
+import { useContext } from 'react';
+import { ModelContext } from "../ModelContext";
+import { StyleSheet, Text, View } from 'react-native';
+
+export default
+    function FrontPage({navigation}) {
+    const model = useContext(ModelContext);
+    const [deviceList, copyDeviceList] = React.useState(model.devices);
+    React.useEffect(wasCreatedACB, []);
+    React.useEffect(deviceListChangedACB, [deviceList]);
+    let ShortDevices = null;
+
+    function observerACB() {
+        copyDeviceList(model.device)
+    }
+
+    function wasCreatedACB() {
+        model.addObserver(observerACB);
+        function isTakenDownACB() {
+            model.removeObserver(observerACB);
+        }
+        return isTakenDownACB;
+    }
+
+    function deviceListChangedACB() {
+        ShortDevices = model.device.map(shortenObjectsCB);
+
+        function shortenObjectsCB(obj) {
+            return {id: obj.id, name: obj.name, moisture: obj.currentMoisture, waterlevel: obj.waterLevel};
+        }
+    }
+
+    function runPumpACB() {
+        console.log("i'm watering! :)")
+    }
+
+    return (
+        <View>
+            <FrontpageView
+                devices = {ShortDevices}
+                runPump = {runPumpACB}
+            >
+            </FrontpageView>
+        </View>
+    )
+}
