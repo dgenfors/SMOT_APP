6530338c318ec91d4da1360247a88613bd0e4561
AlexanderOlegard <zanru.wankelao@gmail.com>
2023-05-05 11:05:32 +0200

    fixed details on mobile, broke it on web :)

--- Views/detailsView.js
+++ Views/detailsView.js
@@ -1,5 +1,5 @@
 
-import { Button, StyleSheet, Text, View, SafeAreaView, TextInput, Pressable } from 'react-native';
+import { Button, StyleSheet, Text, View, SafeAreaView, TextInput, Pressable, ScrollView } from 'react-native';
 import Slider from '@react-native-community/slider';
 
 export default function DetailsView(props) {
@@ -8,7 +8,7 @@ export default function DetailsView(props) {
 
     function nameChanged(event) {
         console.log(event.nativeEvent.text);
-        if(event.nativeEvent.text != "")
+        if (event.nativeEvent.text != "")
             props.onNameChanged(event.nativeEvent.text);
         else
             props.onNameChanged("Unnamed");
@@ -23,56 +23,60 @@ export default function DetailsView(props) {
     }
     return (
         <View style={styles.container}>
-            <View style={styles.infoLine}>
-                <Text style={styles.infoText}>Device name: </Text>
-                <TextInput
-                    style={styles.input}
-                    onSubmitEditing={nameChanged}
-                    placeholder={props.device.name}
-                    maxLength={20}
-                ></TextInput>
-            </View>
-            <View style={styles.infoCol}>
-            <View style={styles.infoLine}>
-                <Text style={styles.infoText}>Prefered moisturelevel: </Text>
-                <Text style={styles.valueText}>{props.device.moistureLevel}</Text>
-            </View>
-                <Slider
-                    value = {props.device.moistureLevel}
-                    style={{ width: '100%', alignSelf: 'center', padding: 24}}
-                    step = {10}
-                    minimumValue={0}
-                    maximumValue={100}
-                    minimumTrackTintColor="#04ADE2"
-                    maximumTrackTintColor="#635147"
-                    onSlidingComplete = {onMoistureTargetChanged}
-                    thumbTintColor="#20EFF4"
-                />
-            </View>
-            <View style={styles.infoLine}>
-                <Text style={styles.infoText}>Current moisture: </Text>
-                <Text style={styles.valueText}>{props.device.currentMoisture} %</Text>
-            </View>
-            <View style={styles.infoLine}>
-                <Text style={styles.infoText}>Waterlevel: </Text>
-                <Text style={styles.valueText}>{props.device.waterLevel}</Text>
-            </View>
-            <View style={styles.infoLine}>
-                <Text style={styles.infoText}>Manual watering amount(seconds): </Text>
-                <Text style={styles.valueText}> {props.device.pumpTime}</Text>
-                <Slider
-                    value = {props.device.pumpTime}
-                    style={{ width: '100%', alignSelf: 'center'}}
-                    step = {1}
-                    minimumValue={0}
-                    maximumValue={20}
-                    minimumTrackTintColor="#04ADE2"
-                    maximumTrackTintColor="#635147"
-                    onSlidingComplete = {wateringTimeChanged}
-                    thumbTintColor="#20EFF4"
-                />
-            </View>
-            
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.infoLine}>
+                    <Text style={styles.infoText}>Device name: </Text>
+                    <TextInput
+                        style={styles.input}
+                        onSubmitEditing={nameChanged}
+                        placeholder={props.device.name}
+                        maxLength={20}
+                    ></TextInput>
+                </View>
+                <View style={styles.infoCol}>
+                    <View style={styles.infoLine}>
+                        <Text style={styles.infoText}>Prefered moisturelevel: </Text>
+                        <Text style={styles.valueText}>{props.device.moistureLevel}</Text>
+                    </View>
+                    <Slider
+                        value={props.device.moistureLevel}
+                        style={{ width: '100%', alignSelf: 'center', padding: 24 }}
+                        step={10}
+                        minimumValue={0}
+                        maximumValue={100}
+                        minimumTrackTintColor="#04ADE2"
+                        maximumTrackTintColor="#635147"
+                        onSlidingComplete={onMoistureTargetChanged}
+                        thumbTintColor="#20EFF4"
+                    />
+                </View>
+                <View style={styles.infoLine}>
+                    <Text style={styles.infoText}>Current moisture: </Text>
+                    <Text style={styles.valueText}>{props.device.currentMoisture} %</Text>
+                </View>
+                <View style={styles.infoCol}>
+                    <View style={styles.infoLine}>
+                        <Text style={styles.infoText}>Manual watering amount(seconds): </Text>
+                        <Text style={styles.valueText}> {props.device.pumpTime}</Text>
+                    </View>
+                    <Slider
+                            value={props.device.pumpTime}
+                            style={{ width: '100%', alignSelf: 'center' }}
+                            step={1}
+                            minimumValue={0}
+                            maximumValue={20}
+                            minimumTrackTintColor="#04ADE2"
+                            maximumTrackTintColor="#635147"
+                            onSlidingComplete={wateringTimeChanged}
+                            thumbTintColor="#20EFF4"
+                        />
+                </View>
+                
+                <View style={styles.infoLine}>
+                    <Text style={styles.infoText}>Waterlevel: </Text>
+                    <Text style={styles.valueText}>{props.device.waterLevel}</Text>
+                </View>
+            </ScrollView>
         </View>
     )
 
@@ -87,13 +91,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'space-evenly'
     },
+    scrollView: {
+    },
     infoLine: {
         flex: 1,
         flexDirection: 'row',
-        
+
         maxWidth: 500,
         width: '100%',
-        
+
         paddingHorizontal: 24,
         paddingVertical: 12,
         marginHorizontal: 4,
@@ -102,22 +108,21 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         alignContent: 'space-between',
         justifyContent: 'space-between',
-        
+
         borderRadius: 20,
         backgroundColor: 'lightblue',
     },
     infoCol: {
         flex: 1,
         flexDirection: 'column',
-        
+
         maxWidth: 500,
         width: '100%',
-        
 
         alignItems: 'center',
         alignContent: 'space-between',
         justifyContent: 'space-between',
-        
+
         borderRadius: 20,
         backgroundColor: 'lightblue',
     },
