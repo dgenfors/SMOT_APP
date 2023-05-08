210ac82bd577cde07cf9e8329c4282a6a6365ac5
dgenfors <96737520+dgenfors@users.noreply.github.com>
2023-05-08 14:05:14 +0200

    updates for PlantInfo

--- Presenter/searchbarPresenter.js
+++ Presenter/searchbarPresenter.js
@@ -3,10 +3,13 @@ import SearchbarView from "../Views/searchbarView"
 import SearchResultView from "../Views/searchResultView"
 import {doPlantSearch} from "../plantSearchApi"
 import promiseNoData from "../promiseNoData"
+import { ModelContext } from "../ModelContext";
+import { useContext } from 'react';
 import React from "react"
 import { Button, StyleSheet, Text, View, SafeAreaView, TextInput, Pressable, TouchableOpacity} from 'react-native';
 
 export default function SearchbarPresenter(navigation){
+    const model = useContext(ModelContext);
     const [promiseState]= React.useState({});
     const [, reRender]= React.useState(); 
     if(Object.keys(promiseState).length == 0){
@@ -17,11 +20,14 @@ export default function SearchbarPresenter(navigation){
     function searchPlant(query){
        resolvePromise(doPlantSearch(query),promiseState, notifySearch);
     }
+    function addPlanToModel(item){
+        model.setPlantInfo(item);
+    }
 
         return (<View><SearchbarView
             searchPlant={searchPlant}>
         </SearchbarView>
-        {promiseNoData(promiseState) || <SearchResultView searchResults={promiseState.data}></SearchResultView>}</View>)
+        {promiseNoData(promiseState) || <SearchResultView searchResults={promiseState.data} addPlant={addPlanToModel}></SearchResultView>}</View>)
     
     
 }
