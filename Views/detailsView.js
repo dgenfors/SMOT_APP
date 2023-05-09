9a82eecc530d07d7cc50d20dd1d14cef33f5d67c
dgenfors <96737520+dgenfors@users.noreply.github.com>
2023-05-09 11:40:23 +0200

    Update detailsView.js

--- Views/detailsView.js
+++ Views/detailsView.js
@@ -103,18 +103,20 @@ export default function DetailsView(props) {
                     </View>
                     <Button title="Calibrate sensor" onPress={onCalibratePressed}></Button>
                 </View>
-                <View style={styles.infoCol}>
+                {!props.device.plant ? (<View style={styles.infoLine}><Text>Add a plant to your device to get more information!</Text></View>):null}
+                {props.device.plant ? (<View style={styles.infoCol}>
                     <Text style={styles.infoText}>Plant common name:</Text>
                     <Text style={styles.infoText}>{props.device.plant.common_name}</Text>
-                </View>
-                <View style={styles.infoCol}>
+                </View>): null}
+                {props.device.plant ?(<View style={styles.infoCol}>
                     <Text style={styles.infoText}>Recommended water frequency:</Text>
                     <Text style={styles.infoText}>{props.device.plant.watering}</Text>
-                </View>
-                <View style={styles.infoCol}>
+                </View>):null}
+                {props.device.plant ? (<View style={styles.infoCol}>
                     <Text style={styles.infoText}>Recommended sunlight:</Text>
                     <Text style={styles.infoText}>{props.device.plant.sunlight[0]}</Text>
-                </View>
+                </View>) : null}
+                
             </ScrollView>
         </View>
     )
